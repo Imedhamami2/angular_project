2d32a038d54d02cb17b9ed962caab5fc1460b029
Med Aloui <aloui.sghair@gmail.com>
2020-11-23 21:18:28 +0100

    sign in

--- src/app/sign-in-form/sign-in-form.component.ts
+++ src/app/sign-in-form/sign-in-form.component.ts
@@ -8,12 +8,31 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class SignInFormComponent implements OnInit {
 
+  email : string;
+  password :string;
+  checkEmail: boolean = true;
+  checkPass: boolean = true;
+
 signIn : boolean = true;
 
   constructor() { }
   @Output() openSignUP = new EventEmitter<boolean>();
 
   ngOnInit(): void {
+  }
+
+  checkEmailFn(){
+    let email = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/);
+    if(email.test(this.email) || this.email.length==0 )
+          this.checkEmail=true;
+        else
+          this.checkEmail=false;
+  }
+  checkPassword(){
+    if(this.password.length>=6 || this.password.length==0 )
+          this.checkPass=true;
+        else
+          this.checkPass=false; 
   }
   setSignIn(){
     this.signIn=false;
