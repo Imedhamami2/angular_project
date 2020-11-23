b58fce371d195a37226bad210243de6a334b872a
Ali Messaadi <Alii.messaadi@gmail.com>
2020-11-23 18:39:34 +0100

    Fix sign up verification

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -20,10 +20,8 @@ export class SignUpFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  checkValue(){
+  checkText(){
     let re = /^[A-Za-z]+$/;
-    let number = new RegExp(/^[0-9]+$/);
-    let email = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/);
         if(re.test(this.firstName) || this.firstName.length==0)
           this.checkFirstName=true;
         else
@@ -37,28 +35,28 @@ export class SignUpFormComponent implements OnInit {
         if(re.test(this.city) || this.city.length==0)
           this.checkCity=true;
         else
-          this.checkCity=false;
-
-        if(email.test(this.email) || this.email.length==0 )
+          this.checkCity=false;  
+  }
+  checkNumber(){
+    let number = new RegExp(/^[0-9]+$/);
+    if(number.test(this.zipCode) || this.zipCode.length==0)
+      this.checkZip=true;
+    else
+      this.checkZip=false;
+  }
+  checkEmailFn(){
+    let email = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/);
+    if(email.test(this.email) || this.email.length==0 )
           this.checkEmail=true;
         else
           this.checkEmail=false;
-
-        if(this.password.length>=6 || this.password.length==0 )
+  }
+  checkPassword(){
+    if(this.password.length>=6 || this.password.length==0 )
           this.checkPass=true;
         else
-          this.checkPass=false;
-
-        if(number.test(this.zipCode) || this.zipCode.length==0)
-          this.checkZip=true;
-        else
-          this.checkZip=false;
-
-
-
-        
-
-        }
+          this.checkPass=false; 
+  }
         setSignUp(){
           this.openSigIN.emit(this.signUp);
         }
