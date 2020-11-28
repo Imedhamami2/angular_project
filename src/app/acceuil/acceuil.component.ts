5c1309c628714303cec9ba157ec935fef94c8f6e
Med Aloui <aloui.sghair@gmail.com>
2020-11-28 17:29:25 +0100

    aaa

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
-export class AcceuilComponent implements OnInit {
+export class AcceuilComponent implements OnInit, OnDestroy {
+
+
+
+  myO : Observable<Array<number>>= Observable.create((observer : Observer<Array<number>>) => {
+    let arr = []
+    setInterval(() => {
+      arr.push(arr.length)
+      observer.next(arr)
+    },90000000)
+    
+
+  })
+  mySub : Subscription
+  myArr : Array<number> = []
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.myO.subscribe(arr => this.myArr = arr)
   }
 
-}
+    ngOnDestroy(){ 
+    }
+
+    }
